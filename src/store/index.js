230789a1a6f9beb989fa64c7fde7096af39eeaa7
xubaoshi <xubaoshi@kedacom.com>
2019-05-05 17:34:54 +0800

    edit service bug

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,25 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import createLogger from 'vuex/dist/logger';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createLogger from 'vuex/dist/logger'
 
-import app from './modules/app';
-import demo from './modules/demo';
+import app from './modules/app'
+import demo from './modules/demo'
 
-import getters from './getters';
+import getters from './getters'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 const storeTree = {
   modules: {
     app,
     demo
   },
   getters
-};
-
-if (process.env.NODE_ENV === 'development') {
-  Object.assign(storeTree, { plugins: [createLogger()] });
 }
 
-const store = new Vuex.Store({ ...storeTree });
+if (process.env.NODE_ENV === 'development') {
+  Object.assign(storeTree, { plugins: [createLogger()] })
+}
 
-export default store;
+const store = new Vuex.Store({ ...storeTree })
+
+export default store
